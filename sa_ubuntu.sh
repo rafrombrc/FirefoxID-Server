e463448fa64abc0d6f701fa78339006830febd51
jrconlin <jconlin+git@mozilla.com>
2011-05-04 14:40:27 -0700

    added fix to accept wrapper args and other fixes
    You will need to run "make build" again to pick up fixed dependency.
    also:
    * Fixed shell call in make_ss_certs.sh
    * moved __debug__ to production.ini (with pointer to docs)
    *   fix to test_api to properly check return
    * removed user info section from admin page (later feature)
    * added sa_ubuntu.sh script as an assitant for stand-alone server.

---
+++ sa_ubuntu.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+if [ "~whoami`" != 'root' ]
+then echo "Please run this command as Root"
+exit
+fi
+INSTALL = apt-get install
+echo "#### Installing extra libraries "
+$INSTALL python-2.6
+$INSTALL python-virtualenv
+$INSTALL libsasl2-dev
+$INSTALL python-cxx-dev
+$INSTALL libldap2-dev
+$INSTALL nginx
+echo "#### Adding HTML directories "
+mkdir -p /var/www
+ln -s /var/www/sample sample
+ln -s var/www/s static
+echo "#### Copying nginx config files."
+cp /etc/nginx/nginx.conf.backup-`date +'%Y%m%d'`
+cp conf/nginx/nginx.conf.UBUNTU /etc/nginx/nginx.conf
+cp conf/nginx/conf.d/* /etc/nginx/conf.d/*
+ln -s /etc/nginx/conf.d /etc/nginx/conf.d/ssl
+echo "#### Making self-signed certs (You\'ll have to do things now)."
+pushd certs
+bash make_ss_certs.sh
+popd
+echo "#### Restarting nginx"
+/etc/init,d/nginx restart
+echo "####"
+echo " With any hope, you now have a properly configured stand alone identity server.
+
+You should be able to go to http://localhost/1/login and see the login page.
+"
